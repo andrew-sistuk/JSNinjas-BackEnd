a070faf122f972b9e58d84dd685959fc515a0dc4
Andrew Sistuk <32619379+andrew-sistuk@users.noreply.github.com>
2024-11-03 21:25:03 +0200

    Add image upload support to character creation and update routes

--- src/routers/characters.js
+++ src/routers/characters.js
@@ -10,6 +10,7 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import { createCharacterSchema, updateCharacterSchema } from '../validation/characters.js';
 import { isValidId } from '../middlewares/isValidId.js';
+import { upload } from '../middlewares/multer.js';
 
 const router = Router();
 
@@ -17,9 +18,9 @@ router.get('/characters', ctrlWrapper(getAllCharactersController));
 
 router.get('/characters/:characterId', isValidId, ctrlWrapper(getCharacterByIDController));
 
-router.post('/characters', validateBody(createCharacterSchema), ctrlWrapper(createCharacterController));
+router.post('/characters', validateBody(createCharacterSchema), upload.array('images', 10), ctrlWrapper(createCharacterController));
 
-router.patch('/characters/:characterId', isValidId, validateBody(updateCharacterSchema), ctrlWrapper(patchCharacterController));
+router.patch('/characters/:characterId', isValidId, validateBody(updateCharacterSchema), upload.array('images', 10), ctrlWrapper(patchCharacterController));
 
 router.delete(
   '/characters/:characterId',
